ee7271424bbcdcf96dfae06fbc62fba76f4f0082
Caio Costa <caio.costa.cj@gmail.com>
2025-01-29 20:37:51 -0300

    LANDING PAGE com HTML, CSS e JAVASCRIPT
    # My Portfolio Website
    
    Yo! What's up? 👋 This is my portfolio website, where I get to show off my skills and experience as a Front-End Developer and Web Analyst. 😎
    
    ## Under the Hood
    
    This website is built with the tried-and-true trio of HTML, CSS, and JavaScript. But it's not all basic building blocks! Here's what makes it tick:
    
    * **Flexbox:** This nifty feature lets me arrange elements on the page in a flexible way, making the website responsive and adaptable to different screen sizes. No matter what device you're using, it'll look sharp!
    * **Media Queries:** These are like magic spells that adjust the website's layout based on the screen size. So, whether you're on a desktop, tablet, or phone, you'll have a smooth user experience.
    * **JavaScript:** This adds a sprinkle of interactivity, like fetching my project data from GitHub and dynamically populating the "Meus projetos" section. Cool, right?
    
    ## Visually Appealing Design
    
    The website boasts a clean and modern design that prioritizes readability and a user-friendly experience. The dark color scheme with contrasting elements makes the content stand out and easy on the eyes.
    
    ## Tech Stack in Action
    
    Here's a quick breakdown of the technologies I used to bring this website to life:
    
    * **Front-End:**
        * HTML: The foundation of the website, providing structure and content.
        * CSS: Styles the website with colors, fonts, and layouts, making it visually appealing.
        * JavaScript: Adds interactivity and fetches data from external APIs.
    * **Other:**
        * Git: A version control system that keeps track of all the changes I make to the website's code.
        * GitHub Pages: A free hosting platform specifically designed for static websites like this one.
    
    ## Let's Connect!
    
    I hope you enjoyed exploring my portfolio website and learned a bit more about me and my work. If you have any questions or feedback, feel free to reach out. I'm always down to connect! 🚀
    
    **Keep learning, keep coding!**
    
    -------------------------------------------
    
    # Meu Portfólio
    
    E aí, beleza? 👋 Esse é meu portfólio, onde você pode conferir minhas habilidades e experiência como Desenvolvedor Front-end e Analista Web. 😎
    
    ## Por debaixo dos Panos
    
    Esse site foi construído com o trio infalível: HTML, CSS e JavaScript. Mas não para por aí! Veja só o que mais usei por aqui:
    
    * **Flexbox:** Essa ferramenta incrível me permite organizar os elementos da página de forma flexível, deixando o site responsivo e adaptável para qualquer tela.
    * **Media Queries:** Essas são como mágicas que ajustam o layout do site de acordo com o tamanho da tela. Assim, seja no computador, tablet ou celular, a experiência é sempre boa.
    * **JavaScript:** Adiciona aquele toque especial de interatividade, como buscar meus projetos do GitHub e mostrar eles aqui na página. Legal, né?
    
    ## Design Atraente
    
    O site tem um design limpo e moderno, priorizando a legibilidade e uma experiência fácil de usar. O esquema de cores escuras com elementos contrastantes faz com que o conteúdo destaque e seja fácil de ler.
    
    ## Tecnologias Utilizadas
    
    Aqui está um resumo das tecnologias que usei para criar esse site:
    
    * **Front-end:**
        * HTML: A base do site, fornecendo estrutura e conteúdo.
        * CSS: Estiliza o site com cores, fontes e layouts, deixando-o visualmente atraente.
        * JavaScript: Adiciona interatividade e busca dados de APIs externas.
    * **Outras:**
        * Git: Um sistema de controle de versão que acompanha todas as mudanças que faço no código do site.
        * GitHub Pages: Uma plataforma de hospedagem gratuita especialmente projetada para sites estáticos como este.
    
    ## Vamos Conectar!
    
    Espero que você tenha gostado de explorar meu portfólio e aprendido um pouco mais sobre mim e meu trabalho. Se tiver alguma dúvida ou feedback, fique à vontade para entrar em contato. Estou sempre aberto a conversar! 🚀
    
    **Continue aprendendo, continue codando!**

---
+++ main.js
@@ -0,0 +1,34 @@
+function getProjects() {
+    const baseUrl = 'https://api.github.com/repos/caiojcosta/';
+    const projectUrls = ['Alura-Back-End', 'caio', 'Flappy-Bird', 'ATIVIDADES'];
+    const projectPromises = projectUrls.map(projectUrl => fetch(`${baseUrl}${projectUrl}`, { method: 'GET' }));
+    var loadingElement = document.getElementById('loading');
+
+    Promise.all(projectPromises)
+        .then(responses => Promise.all(responses.map(response => response.json())))
+        .then(projectsData => {
+            // Call showProject function with all projects data
+            loadingElement.style.display = 'none';
+            showProject(projectsData);
+            console.log(projectsData);
+        })
+        .catch(error => {
+            console.error('Error fetching projects:', error);
+        });
+}
+
+function showProject(data) {
+    var listElement = document.getElementById('my-projects-list')
+
+    for (let i = 0; i < data.length; i++)
+    {
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+}
+    getProjects();
